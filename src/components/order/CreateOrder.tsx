2841a07b9aa1066c9857aa61bca031c43e101cca
okste1234 <okste1234@gmail.com>
2024-09-27 04:04:38 +0100

    CHORE: adding proper margin at create order

--- src/components/order/CreateOrder.tsx
+++ src/components/order/CreateOrder.tsx
@@ -6,7 +6,7 @@ export const CreateOrder = () => {
       <div className="mt-12">
         <div className="bg-white rounded-xl text-[#262626] p-2 w-44 h-72 m-auto">
         <div className="rounded-xl border-2 border-dashed border-[#262626] w-full h-full flex flex-col items-center justify-center">
-            <div className="">
+            <div className="mb-1">
              <Image src={"/plus.svg"} alt="token" width={24} height={24} priority quality={100} />
             </div>
             <div className="flex flex-col items-center">
