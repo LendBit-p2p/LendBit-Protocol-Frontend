3c6e50e0c343688493cb0a4e7e31210e192d3de8
okste1234 <okste1234@gmail.com>
2024-10-07 10:01:07 +0100

    UPDATE: diamod abi added, env variables configured

---
+++ src/constants/envVar.ts
@@ -0,0 +1,8 @@
+export const envVars = {
+    
+    protocolDiamondAddress: process.env.NEXT_PUBLIC_Protocol_Diamond_Contract,
+
+    httpRPC: process.env.NEXT_PUBLIC_HTTP_RPC,
+
+    wssRPC: process.env.NEXT_PUBLIC_WEBSOCKET_RPC,
+};
