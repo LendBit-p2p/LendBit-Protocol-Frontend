1a30776e2e9626bcf960dee40b8da26a74789584
SonOfUri <alotimothy01@gmail.com>
2024-10-16 04:18:55 +0100

    update btn

--- src/components/market/CardLayout.tsx
+++ src/components/market/CardLayout.tsx
@@ -10,6 +10,18 @@ import useServiceRequest from "@/hooks/useServiceRequest";
 import { useRouter } from "next/navigation";
 import useGetValueAndHealth from "@/hooks/useGetValueAndHealth";
 
+interface BtnProps {
+    text: string;
+    css?: string;
+    onClick?: () => void;
+}
+
+export const Btn: React.FC<BtnProps> = ({ text, css, onClick }) => (
+    <button className={css} onClick={onClick}>
+        {text}
+    </button>
+);
+
 const tokenImageMap: { [key: string]: { image: string; label: string } } = {
     "0xE4aB69C077896252FAFBD49EFD26B5D171A32410": { image: "/link.svg", label: "LINK" },
     "0x0000000000000000000000000000000000000001": { image: "/eth.svg", label: "ETH" },
